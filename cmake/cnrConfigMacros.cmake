dd7563304dd57358b85cb6a0ff160cdb1afe54fe
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2024-07-02 21:59:08 +0200

    full control of rpath

--- cmake/cnrConfigMacros.cmake
+++ cmake/cnrConfigMacros.cmake
@@ -73,18 +73,6 @@ macro(cnr_set_flags)
   if(${CMAKE_VERSION} VERSION_GREATER "3.16.0")
     set(THREADS_PREFER_PTHREAD_FLAG ON)
   endif()
-
-  # use, i.e. don't skip the full RPATH for the build tree
-  set(CMAKE_SKIP_BUILD_RPATH FALSE)
-
-  # when building, don't use the install RPATH already
-  # (but later on when installing)
-  set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
- 
-  # add the automatically determined parts of the RPATH
-  # which point to directories outside the build tree to the install RPATH
-  set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 endmacro()
 
 #
@@ -112,6 +100,94 @@ macro(cnr_target_compile_options TARGET_NAME)
     target_compile_options(${TARGET_NAME} PRIVATE /W3)
 
   endif()
+endmacro()
+
+#
+# cnr_install
+#
+macro(
+  cnr_install
+  LIBRARY_TARGETS_LIST
+  EXECUTABLE_TARGETS_LIST
+  HEADERS_DIRS)
+
+  list(APPEND TARGETS_LIST ${LIBRARY_TARGETS_LIST} ${EXECUTABLE_TARGETS_LIST})
+  
+  # We create the congfig files for the package
+  set(CONFIG_INSTALL_DIR_ALTERNATIVE "${CONFIG_INSTALL_DIR}/../cmake_alternative")
+  set(CONFIG_NAMESPACE               "${PROJECT_NAME}::")
+  set(TARGETS_EXPORT_NAME            "${PROJECT_NAME}Targets")
+  set(VERSION_CONFIG                 "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake")
+  set(PROJECT_CONFIG_OUTPUT          "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake")
+  set(PROJECT_CONFIG_INPUT_TEMPLATE  "cmake/cnrConfigTemplate.cmake.in")
+
+  # Parameter template in the PROJECT_CONFIG_INPUT_TEMPLATE
+  set(EXPORTED_LIBRARY_TARGETS_LIST     "${LIBRARY_TARGETS_LIST}")
+  set(EXPORTED_EXECUTABLE_TARGETS_LIST  "${EXECUTABLE_TARGETS_LIST}")
+  set(EXPORTED_LIBRARY_TARGET_RPATH     "${PACKAGE_LIB_DESTINATION}")
+  
+  ##
+  message(STATUS "CONFIG_INSTALL_DIR              = ${CONFIG_INSTALL_DIR}"           )
+  message(STATUS "CONFIG_NAMESPACE                = ${CONFIG_NAMESPACE}"             )
+  message(STATUS "TARGETS_EXPORT_NAME             = ${TARGETS_EXPORT_NAME}"          )
+  message(STATUS "VERSION_CONFIG                  = ${VERSION_CONFIG}"               )
+  message(STATUS "PROJECT_CONFIG_OUTPUT           = ${PROJECT_CONFIG_OUTPUT}"        )
+  message(STATUS "PROJECT_CONFIG_INPUT_TEMPLATE   = ${PROJECT_CONFIG_INPUT_TEMPLATE}")
+  message(STATUS "PACKAGE_INCLUDE_DESTINATION     = ${PACKAGE_INCLUDE_DESTINATION}"  )
+  message(STATUS "EXPORTED_LIBRARY_TARGETS_LIST   = ${LIBRARY_TARGETS_LIST}"         )
+  message(STATUS "EXPORTED_EXECUTABLE_TARGETS_LIST= ${EXECUTABLE_TARGETS_LIST}"      )
+
+  # 1 install files
+  foreach(HEADERS_DIR ${HEADERS_DIRS})
+    install(
+      DIRECTORY ${HEADERS_DIR}/
+      DESTINATION "${PACKAGE_INCLUDE_DESTINATION}"
+      FILES_MATCHING
+      PATTERN "*.h"
+      PATTERN "*.hpp"
+      PATTERN ".svn" EXCLUDE)
+  endforeach()
+
+  install(
+    TARGETS ${TARGETS_LIST}
+    EXPORT ${TARGETS_EXPORT_NAME}
+    ARCHIVE DESTINATION "${PACKAGE_LIB_DESTINATION}"
+    LIBRARY DESTINATION "${PACKAGE_LIB_DESTINATION}"
+    RUNTIME DESTINATION "${PACKAGE_BIN_DESTINATION}")
+
+  include(CMakePackageConfigHelpers)
+
+  #------------------------------------------------------------------------------
+  # Configure <PROJECT_NAME>ConfigVersion.cmake common to build and install tree
+  write_basic_package_version_file(
+    "${VERSION_CONFIG}"
+    VERSION ${extracted_version}
+    COMPATIBILITY SameMajorVersion)
+
+  #------------------------------------------------------------------------------
+  # Create the ${PROJECT_NAME}Config.cmake using the template
+  # ${PROJECT_NAME}Config.cmake.in
+  configure_package_config_file(
+    "${PROJECT_CONFIG_INPUT_TEMPLATE}" "${PROJECT_CONFIG_OUTPUT}"
+    INSTALL_DESTINATION "${CONFIG_INSTALL_DIR}")
+
+  #------------------------------------------------------------------------------
+  # Install cmake targets files
+  install(
+    EXPORT "${TARGETS_EXPORT_NAME}"
+    NAMESPACE "${CONFIG_NAMESPACE}"
+    DESTINATION "${CONFIG_INSTALL_DIR}"
+    FILE ${TARGETS_EXPORT_NAME}.cmake
+  )
+  
+  #------------------------------------------------------------------------------
+  # Install cmake config files
+  install(FILES "${PROJECT_CONFIG_OUTPUT}" "${VERSION_CONFIG}"
+          DESTINATION "${CONFIG_INSTALL_DIR}")
+
+  install(FILES "${PROJECT_CONFIG_OUTPUT}" "${VERSION_CONFIG}"
+          DESTINATION "${CONFIG_INSTALL_DIR_ALTERNATIVE}")
+
 endmacro()
 
 #
@@ -157,87 +233,3 @@ macro(cnr_configure_gtest trg deps)
 
   endif()
 endmacro()
-
-#
-# cnr_install
-#
-macro(
-  cnr_install
-  PROJECT_VERSION
-  LIBRARY_TARGETS_LIST
-  EXECUTABLE_TARGETS_LIST
-  HEADERS_DIRS)
-
-  list(APPEND TARGETS_LIST ${LIBRARY_TARGETS_LIST} ${EXECUTABLE_TARGETS_LIST})
-  
-  ###################################################################
-  set(CONFIG_NAMESPACE                "${PROJECT_NAME}::")
-  set(TARGETS_EXPORT_NAME             "${PROJECT_NAME}Targets")
-  set(VERSION_CONFIG                  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake")
-  set(PROJECT_CONFIG_OUTPUT           "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake")
-  set(PROJECT_CONFIG_INPUT_TEMPLATE   "cmake/cnrConfigTemplate.cmake.in")
-
-  # Parameter template in the PROJECT_CONFIG_INPUT_TEMPLATE
-  set(PACKAGE_INCLUDE_INSTALL_DIR       "${CMAKE_INSTALL_PREFIX}/include")
-  set(EXPORTED_LIBRARY_TARGETS_LIST     "${LIBRARY_TARGETS_LIST}")
-  set(EXPORTED_EXECUTABLE_TARGETS_LIST  "${EXECUTABLE_TARGETS_LIST}")
-  set(EXPORTED_LIBRARY_TARGET_RPATH     "${PACKAGE_LIB_DESTINATION}")
-  
-  ##
-  message(STATUS "CONFIG_INSTALL_DIR=${CONFIG_INSTALL_DIR}")
-  message(STATUS "CONFIG_NAMESPACE=${CONFIG_NAMESPACE}")
-  message(STATUS "TARGETS_EXPORT_NAME=${TARGETS_EXPORT_NAME}")
-  message(STATUS "VERSION_CONFIG=${VERSION_CONFIG}")
-  message(STATUS "PROJECT_CONFIG_OUTPUT=${PROJECT_CONFIG_OUTPUT}")
-  message(STATUS "PROJECT_CONFIG_INPUT_TEMPLATE=${PROJECT_CONFIG_INPUT_TEMPLATE}")
-  message(STATUS "PACKAGE_INCLUDE_INSTALL_DIR=${PACKAGE_INCLUDE_INSTALL_DIR}")
-  message(STATUS "LIBRARY_TARGETS_LIST=${LIBRARY_TARGETS_LIST}")
-  message(STATUS "EXPORTED_EXECUTABLE_TARGETS_LIST=${EXECUTABLE_TARGETS_LIST}")
-  message(STATUS "LIBRARY_TARGETS_LIST=${LIBRARY_TARGETS_LIST}")
-  message(STATUS "TARGETS_LIST=${TARGETS_LIST}")
-  
-  include(CMakePackageConfigHelpers)
-
-  # Create the ${PROJECT_NAME}ConfigVersion.cmake
-  write_basic_package_version_file(
-    "${VERSION_CONFIG}"
-    VERSION ${PROJECT_VERSION}
-    COMPATIBILITY SameMajorVersion)
-
-  # Create the ${PROJECT_NAME}Config.cmake using the template
-  # ${PROJECT_NAME}Config.cmake.in
-  configure_package_config_file(
-    "${PROJECT_CONFIG_INPUT_TEMPLATE}" "${PROJECT_CONFIG_OUTPUT}"
-    INSTALL_DESTINATION "${CONFIG_INSTALL_DIR}")
-
-  # Install cmake config files
-  install(FILES "${PROJECT_CONFIG_OUTPUT}" "${VERSION_CONFIG}"
-          DESTINATION "${CONFIG_INSTALL_DIR}")
-          
-  install(
-    TARGETS ${TARGETS_LIST}
-    EXPORT ${TARGETS_EXPORT_NAME}
-    ARCHIVE DESTINATION "${PACKAGE_LIB_DESTINATION}"
-    LIBRARY DESTINATION "${PACKAGE_LIB_DESTINATION}"
-    RUNTIME DESTINATION "${PACKAGE_BIN_DESTINATION}")
-
-  foreach(HEADERS_DIR ${HEADERS_DIRS})
-    install(
-      DIRECTORY ${HEADERS_DIR}/
-      DESTINATION "${PACKAGE_INCLUDE_DESTINATION}"
-      FILES_MATCHING
-      PATTERN "*.h"
-      PATTERN "*.hpp"
-      PATTERN ".svn" EXCLUDE)
-  endforeach()
-
-  # Install cmake targets files
-  install(
-    EXPORT ${TARGETS_EXPORT_NAME}
-    NAMESPACE "${CONFIG_NAMESPACE}"
-    DESTINATION "${CONFIG_INSTALL_DIR}"
-    FILE ${PROJECT_NAME}Targets.cmake
-  )
-  ###################################################################
-endmacro()
-
